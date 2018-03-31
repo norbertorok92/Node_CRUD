d4ec0b0b285540843b4def09e83272c090447e23
Norbert Torok <ntorok@grubhub.com>
2018-03-31 22:11:40 +0300

    Push Node CRUD API to master

---
+++ config/db.js
@@ -0,0 +1,4 @@
+module.exports = {
+  // Insert your mlab/mongodb user and password below (e.g.: "mongodb://johndoe:password@.....")
+  url : "mongodb://user:password@ds215739.mlab.com:15739/persmap"
+};
